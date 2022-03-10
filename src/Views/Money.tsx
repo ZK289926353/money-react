7786b2e2a95f566f0a4387cb02efe23023f6f184
Zkk <289926353@qq.com>
2022-03-10 11:31:17 +0800

    备注style完成

--- src/Views/Money.tsx
+++ src/Views/Money.tsx
@@ -2,10 +2,10 @@ import Layout from "Components/Layout";
 import styled from "styled-components";
 
 const TagsSection = styled.section`
-  background: #ffffff;
-  padding:12px 16px;
+  background: #fff;
+  padding: 12px 16px;
   > ol {
-    margin-left:-12px;
+    margin-left: -12px;
     > li {
       background: #d9d9d9;
       border-radius: 18px;
@@ -15,17 +15,36 @@ const TagsSection = styled.section`
       margin: 8px 12px;
     }
   }
-  >button{
-    background:none;
-    border:none;
-    padding:2px 4px;
-    border-bottom:1px solid #333;
-    color:#666;
-    margin-top:8px;
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
   }
 `;
 
-const NotesSection = styled.section``;
+const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding:0px  16px;
+  font-size:14px;
+  >label{
+    display:flex;
+    align-items:center;
+    >span{
+      margin-right:16px;
+      white-space:nowrap;
+    }
+    >input{
+      display:block;
+      width:100%;
+      height:72px;
+      background:none;
+      border:none;
+    }
+  }
+`;
 const CategorySection = styled.section``;
 const NumberPadSection = styled.section``;
 function Money() {
@@ -43,7 +62,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text" />
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
