2839277904e4cc9a466770eeff082731fe7c45ff
Zkk <289926353@qq.com>
2022-03-10 11:24:24 +0800

    Money.tsx的style

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,30 @@ import {
   Route,
   Routes
 } from "react-router-dom";
+import styled from "styled-components";
 import Money from "Views/Money";
 import NoMatch from "Views/NoMatch";
 import Statistic from "Views/Statistic";
 import Tags from "Views/Tags";
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/tags" element={<Tags />}></Route>
-        <Route path="/money" element={<Money />}></Route>
-        <Route path="/statistic" element={<Statistic />}></Route>
-        <Route path="/" element={<Navigate to="/money" />}></Route>
-        <Route path="*" element={<NoMatch />}></Route>
-      </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route path="/tags" element={<Tags />}></Route>
+          <Route path="/money" element={<Money />}></Route>
+          <Route path="/statistic" element={<Statistic />}></Route>
+          <Route path="/" element={<Navigate to="/money" />}></Route>
+          <Route path="*" element={<NoMatch />}></Route>
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
-
-
-
-
-
-
 
 export default App;
