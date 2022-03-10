7e014700540b8dd8b7e5d8603d7714a594f1078f
SadMuMu <289926353@qq.com>
2022-03-10 21:04:27 +0800

    重构NumberPadSection

--- src/Views/Money/NumberPadSection.tsx
+++ src/Views/Money/NumberPadSection.tsx
@@ -1,67 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output {
-    background: #fff;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-  > .pad {
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    grid-template-rows: repeat(4, 25%);
-    > button {
-      font-size: 18px;
-      width: 100%;
-      height: 64px;
-      border: none;
-      &.ok {
-        height: 128px;
-        grid-column: 4/5;
-        grid-row: 3/5;
-      }
-      &.zero {
-        grid-column: 1/3;
-        grid-row: 4/5;
-      }
-      &:nth-child(1) {
-        background: #f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #e0e0e0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #d3d3d3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #c1c1c1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #bbbbbb;
-      }
-      &:nth-child(12) {
-        background: #9a9a9a;
-      }
-      &:nth-child(14) {
-        background: #a9a9a9;
-      }
-    }
-  }
-`;
+import { generateOutput } from "./NumberPadSection/generateOutput";
+import { Wrapper } from "./NumberPadSection/Wrapper";
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = React.useState("0");
@@ -75,42 +15,15 @@ const NumberPadSection: React.FC = () => {
   };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
-    switch (text) {
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        if (output === "0") {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case ".":
-        if (output.indexOf(".") >= 0) {
-          return;
-        }
-        setOutput(output + ".");
-        break;
-      case "删除":
-        if (output.length === 1) {
-          setOutput("");
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case "清空":
-        setOutput("");
-        break;
-      case "OK":
-        console.log("ok");
-        break;
+    if (text === null) {
+      return;
+    }
+    if (text === "OK") {
+      // TODO
+      return;
+    }
+    if ("0123456789.".split("").concat(["删除", "清空"]).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output));
     }
   };
   return (
