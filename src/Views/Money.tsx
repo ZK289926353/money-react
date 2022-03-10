4c007b0bb2a6d5ac0bb553913497e475173df0ad
Zkk <289926353@qq.com>
2022-03-10 14:29:39 +0800

    数字键盘style修改

--- src/Views/Money.tsx
+++ src/Views/Money.tsx
@@ -27,48 +27,107 @@ const TagsSection = styled.section`
 
 const NotesSection = styled.section`
   background: #f5f5f5;
-  padding:0px  16px;
-  font-size:14px;
-  >label{
-    display:flex;
-    align-items:center;
-    >span{
-      margin-right:16px;
-      white-space:nowrap;
+  padding: 0px 16px;
+  font-size: 14px;
+  > label {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
     }
-    >input{
-      display:block;
-      width:100%;
-      height:72px;
-      background:none;
-      border:none;
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
     }
   }
 `;
 const CategorySection = styled.section`
-font-size:24px;
-  >ul{
-    display:flex;
-    background:#c4c4c4;
-    >li{
-      width:50%;
-      text-align:center;
-      padding:16px 0;
-      position:relative;
-      &.selected::after{
+  font-size: 24px;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after {
         content: "";
-        display:block;
-        height:3px;
-        background:#333;
-        position:absolute;
-        bottom:0;
-        width:100%;
-        left:0;
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
       }
     }
   }
 `;
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background: #fff;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+  >.pad{
+    display:grid;
+    grid-template-columns: repeat(4,25%);
+    grid-template-rows:  repeat(4,25%);
+    >button{
+      font-size:18px;
+      width:100%;
+      height:64px;
+      border:none;
+      &.ok{
+        height: 128px;
+        grid-column:4/5;
+        grid-row:3/5;
+      }
+      &.zero{
+        grid-column:1/3;
+        grid-row:4/5;
+      }
+      &:nth-child(1){
+        background:#F2F2F2;
+      }
+      &:nth-child(2),
+      &:nth-child(5){
+        background:#E0E0E0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9){
+        background:#D3D3D3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10){
+        background:#C1C1C1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13){
+        background:#BBBBBB;
+      }
+      &:nth-child(12){
+        background:#9A9A9A;
+      }
+      &:nth-child(14){
+        background:#A9A9A9;
+      }
+    }
+  }
+`;
 function Money() {
   return (
     <Layout>
@@ -84,7 +143,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
+          <input type="text" placeholder="在这里添加备注" />
         </label>
       </NotesSection>
       <CategorySection>
@@ -94,21 +153,23 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>OK</button>
-        <button>0</button>
-        <button>.</button>
+        <div className="output" >100</div>
+        <div className="pad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button>.</button>
+        </div>
       </NumberPadSection>
     </Layout>
   );
