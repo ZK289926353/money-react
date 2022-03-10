94bab10c7178f5d2e5c0b2db7cb9b99dc0a8829d
SadMuMu <289926353@qq.com>
2022-03-10 20:13:58 +0800

    支出收入选择完成

--- src/Views/Money/CategorySection.tsx
+++ src/Views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const CategorySection = styled.section`
+const _CategorySection = styled.section`
   font-size: 24px;
   > ul {
     display: flex;
@@ -23,5 +24,25 @@ const CategorySection = styled.section`
     }
   }
 `;
+
+const CategorySection: React.FC = () => {
+  const categoryMap={'-':'支出','+':'收入'};
+  type Y= keyof typeof categoryMap;
+  const [categoryList] = useState<Y[]>(['-', '+']);
+  const [category, setCategory] = React.useState("-");
+  return (
+    <_CategorySection>
+      <ul>
+        {categoryList.map((c) => (
+          <li key={c}
+            className={category === c ? "selected" : ""}
+            onClick={() => {setCategory(c)}}
+          >{categoryMap[c]}
+          </li>
+        ))}
+      </ul>
+    </_CategorySection>
+  );
+};
 
 export { CategorySection };
--- src/Views/Money/NumberPadSection.tsx
+++ src/Views/Money/NumberPadSection.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   > .output {
@@ -9,56 +9,81 @@ const NumberPadSection = styled.section`
     line-height: 72px;
     text-align: right;
     padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
   }
-  >.pad{
-    display:grid;
-    grid-template-columns: repeat(4,25%);
-    grid-template-rows:  repeat(4,25%);
-    >button{
-      font-size:18px;
-      width:100%;
-      height:64px;
-      border:none;
-      &.ok{
+  > .pad {
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: repeat(4, 25%);
+    > button {
+      font-size: 18px;
+      width: 100%;
+      height: 64px;
+      border: none;
+      &.ok {
         height: 128px;
-        grid-column:4/5;
-        grid-row:3/5;
+        grid-column: 4/5;
+        grid-row: 3/5;
       }
-      &.zero{
-        grid-column:1/3;
-        grid-row:4/5;
+      &.zero {
+        grid-column: 1/3;
+        grid-row: 4/5;
       }
-      &:nth-child(1){
-        background:#F2F2F2;
+      &:nth-child(1) {
+        background: #f2f2f2;
       }
       &:nth-child(2),
-      &:nth-child(5){
-        background:#E0E0E0;
+      &:nth-child(5) {
+        background: #e0e0e0;
       }
       &:nth-child(3),
       &:nth-child(6),
-      &:nth-child(9){
-        background:#D3D3D3;
+      &:nth-child(9) {
+        background: #d3d3d3;
       }
       &:nth-child(4),
       &:nth-child(7),
-      &:nth-child(10){
-        background:#C1C1C1;
+      &:nth-child(10) {
+        background: #c1c1c1;
       }
       &:nth-child(8),
       &:nth-child(11),
-      &:nth-child(13){
-        background:#BBBBBB;
+      &:nth-child(13) {
+        background: #bbbbbb;
       }
-      &:nth-child(12){
-        background:#9A9A9A;
+      &:nth-child(12) {
+        background: #9a9a9a;
       }
-      &:nth-child(14){
-        background:#A9A9A9;
+      &:nth-child(14) {
+        background: #a9a9a9;
       }
     }
   }
 `;
+
+const NumberPadSection: React.FC = () => {
+  return (
+    <Wrapper>
+      <div className="output">100</div>
+      <div className="pad clearfix">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  );
+};
 
 export { NumberPadSection };
