191bb34ca78d2affff51e2afebcf0b66bce921c8
SadMuMu <289926353@qq.com>
2022-03-13 09:54:34 +0800

    tag list 样式做完

--- src/Views/Tags.tsx
+++ src/Views/Tags.tsx
@@ -1,15 +1,61 @@
+import Icon from "Components/Icon";
 import Layout from "Components/Layout";
+import styled from "styled-components";
 import { useTags } from "useTags";
 
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: white;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Space = styled.div`
+  height: 16px;
+`;
+
 function Tags() {
- const {tags,setTags}=useTags();
+  const { tags, setTags } = useTags();
   return (
-      <Layout>
-        <ul>
-          {tags.map(tag=>
-            <li key={tag}>{tag}</li>)}
-        </ul>
-      </Layout>
+    <Layout>
+      <TagList>
+        {tags.map((tag) => (
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right" />
+          </li>
+        ))}
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>新增标签</Button>
+      </Center>
+    </Layout>
   );
 }
 
