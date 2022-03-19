4725e4988cf529c5ed86365e4393124bf6f330d5
SadMuMu <289926353@qq.com>
2022-03-19 11:41:29 +0800

    update package.json

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 yarn build &&
 cd build &&
-git init&&
-git add. &&
+git init &&
+git add . &&
 git commit -m 'deploy' &&
 git remote add origin https://github.com/ZK289926353/money-react.git  &&
 git push -u origin master -f
