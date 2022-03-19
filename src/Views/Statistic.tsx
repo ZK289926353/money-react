d84063448c19ac6b7fbf9f17f32235b50cf1bf12
SadMuMu <289926353@qq.com>
2022-03-19 10:28:40 +0800

    统计页按时间统计

--- src/Views/Statistic.tsx
+++ src/Views/Statistic.tsx
@@ -1,7 +1,7 @@
 import Layout from "Components/Layout";
-import { useRecords } from "hooks/useRecords";
+import { useRecords, RecordItem } from "hooks/useRecords";
 import { useTags } from "hooks/useTags";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import styled from "styled-components";
 import { CategorySection } from "./Money/CategorySection";
 import day from "dayjs";
@@ -24,34 +24,68 @@ const Item = styled.div`
   }
 `;
 
+const Header = styled.h3`
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+`;
+
 function Statistic() {
   const [category, setCategory] = useState<"-" | "+">("-");
   const { records } = useRecords();
   const { getName } = useTags();
+  const hash: { [K: string]: RecordItem[] } = {};
+  const selectedRecords = records.filter((r) => r.category === category);
+  selectedRecords.forEach((r) => {
+    const key = day(r.createAt).format("YYYY年MM月DD日");
+    if (!(key in hash)) {
+      hash[key] = [];
+    }
+    hash[key].push(r);
+  });
+  const array = Object.entries(hash).sort((a, b) => {
+    if (a[0] === b[0]) return 0;
+    if (a[0] < b[0]) return -1;
+    if (a[0] > b[0]) return 1;
+    return 0;
+  });
+
   return (
     <Layout>
-      <CategorySection
-        value={category}
-        onChange={(value) => {
-          setCategory(value);
-        }}
-      />
-      <div>
-        {records.map((r) => {
-          return (
-            <Item>
-              <div className="tags">
-                {r.tagIds.map((tagId) => (
-                  <span>{getName(tagId)}</span>
-                ))}
-              </div>
-              {r.note && <div className="note">{r.note}</div>}
-              <div className="amount">￥{r.amount}</div>
-              {/* {day(r.createAt).format("YYYY年MM月DD日")} */}
-            </Item>
-          );
-        })}
-      </div>
+      <CategoryWrapper>
+        <CategorySection
+          value={category}
+          onChange={(value) => {
+            setCategory(value);
+          }}
+        />
+      </CategoryWrapper>
+      {array.map(([date, records]) => (
+        <div key={date}>
+          <Header>{date}</Header>
+          <div>
+            {records.map((r) => {
+              return (
+                <Item key={r.createAt}>
+                  <div className="tags oneLine">
+                    {r.tagIds
+                      .map((tagId) => <span key={tagId}>{getName(tagId)}</span>)
+                      .reduce(
+                        (result, span, index, array) =>
+                          result.concat(
+                            index < array.length - 1 ? [span, "，"] : [span]
+                          ),
+                        [] as ReactNode[]
+                      )}
+                  </div>
+                  {r.note && <div className="note">{r.note}</div>}
+                  <div className="amount">￥{r.amount}</div>
+                </Item>
+              );
+            })}
+          </div>
+        </div>
+      ))}
     </Layout>
   );
 }
